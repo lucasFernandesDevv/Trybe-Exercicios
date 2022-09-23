6815443059bff8c522154ef30a735f6c8c46f6c4
lucas <lucas.fernandes3478@gmail.com>
2022-09-23 16:26:07 -0300

    inicio exercicios diarios

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-2-JavaScript-array-e-loopFor/scripts.js
@@ -0,0 +1,27 @@
+let numbers = [100]
+
+// exercício 1
+// for(let index=0;index<numbers.length;index+=1){
+//     console.log(numbers[index])
+// }
+
+//exercicio2
+let contador = 0
+
+ for(let index=0;index<numbers.length;index+=1){
+     contador +=numbers[index]
+    }
+    console.log (contador)
+
+
+// const n = 9 ;
+
+// let resultado = 100;
+// for (let index = 0; index <= n; index += 1) {
+//   resultado -= index;
+// };
+// console.log(resultado);
+
+
+
+
