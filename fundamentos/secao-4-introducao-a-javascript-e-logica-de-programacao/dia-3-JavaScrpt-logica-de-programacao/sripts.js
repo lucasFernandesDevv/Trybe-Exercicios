59c93cbb24ef7b30d1b9f0b39277546d6594c5ee
lucas <lucas.fernandes3478@gmail.com>
2022-09-23 19:16:31 -0300

    add com modo .reverse

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-JavaScrpt-logica-de-programacao/sripts.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-JavaScrpt-logica-de-programacao/sripts.js
@@ -1,4 +1,8 @@
-let word = 'tryber'
+// let word = ['t','r','y','b','e','r']
+// word.reverse();
+// console.log(word)
+
+let word = 'trybe'
 let reverso = []
 for(index=word.length-1;index>=0;index -=1){
      reverso.push(word[index])
