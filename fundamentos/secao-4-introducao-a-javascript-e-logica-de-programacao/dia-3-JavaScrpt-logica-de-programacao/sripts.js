93e9394abd90bc553de329ab8ebe507b99928be0
lucas <lucas.fernandes3478@gmail.com>
2022-09-23 18:54:21 -0300

    exercicio de reverso

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-JavaScrpt-logica-de-programacao/sripts.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-JavaScrpt-logica-de-programacao/sripts.js
@@ -1,5 +1,6 @@
 let word = 'trybe'
-let reverso = ''
-for(index=word.length-1;index>=0;index +=1){
-  reverso += word[index]
+let reverso = []
+for(index=word.length-1;index>=0;index -=1){
+     reverso.push(word[index])
 }console.log(reverso)
+
