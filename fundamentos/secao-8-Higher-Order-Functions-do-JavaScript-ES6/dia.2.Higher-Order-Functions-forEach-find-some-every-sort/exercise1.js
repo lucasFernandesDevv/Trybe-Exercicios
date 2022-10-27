96334e7365e3fc9a07ae51a09bfdf19833e43c74
lucas <lucas.fernandes3478@gmail.com>
2022-10-27 18:29:14 -0300

    add

--- fundamentos/secao-8-Higher-Order-Functions-do-JavaScript-ES6/dia.2.Higher-Order-Functions-forEach-find-some-every-sort/exercise1.js
+++ fundamentos/secao-8-Higher-Order-Functions-do-JavaScript-ES6/dia.2.Higher-Order-Functions-forEach-find-some-every-sort/exercise1.js
@@ -62,3 +62,16 @@ const books = [
   ];
   
   // Adicione o código do exercício aqui:
+  const authorBornIn1947 = () =>  books.find(book => book.author.birthYear === 1947).author.name
+  console.log(authorBornIn1947())
+
+  const smallerName = () => {
+    let nameBook
+    books.forEach((book) => {
+        if(!nameBook || book.name.length < nameBook.length) {
+            nameBook = book.name
+        }
+    })
+    return nameBook
+  } 
+  console.log(smallerName())
